81e47e00bbf6f6d1f42ffbeeef607aff3d697d90
Nandini Jirobe <95235229+nandinijirobe@users.noreply.github.com>
2025-11-10 16:08:32 -0600

    basic quiz functionality working

--- update.py
+++ update.py
@@ -1,19 +1,51 @@
 from mistyPy.GenerateRobot import RobotGenerator
 from mistyPy.Robot import Robot
+import json
 
 
-def mt5_question1 ():
-        for i in range (10):
-                pitch_no = 0.0
-                misty.speak(text= "I spy with my little eye", pitch= pitch_no)
-                pitch_no += 0.1
+def mt5_quiz_session():
+        # load quiz data from JSON file
+        quiz_data = {}
+        with open('mt5_quiz.json') as f:
+                quiz_data = json.load(f)
+
+        # start quiz session
+        print("Starting MT5 Quiz Session...")
+        question_no = 0
+
+        # ask if participants wants to start the quiz
+        misty.speak(quiz_data["questions"][question_no])
+        user_input = int(input()) # 1 if yes, 0 if no
+        if user_input == 1:
+                misty.speak(quiz_data["correct_feedback"][question_no])
+                misty.speak(quiz_data["transitions"][question_no])
+                question_no += 1
+        else:
+                misty.speak(quiz_data["incorrect_feedback"][question_no])
+                print("Quiz session ended because participant refused to play.")
+                return
+        
+        # begin quiz
+        print("Starting to ask participant questions...")
+        while user_input != "-1":
+                misty.speak(quiz_data["questions"][question_no])
+                user_input = int(input())
+                if user_input == 1:
+                        misty.speak(quiz_data["correct_feedback"][question_no])
+                        misty.speak(quiz_data["transitions"][question_no])
+                        question_no += 1
+                else:
+                        misty.speak(quiz_data["incorrect_feedback"][question_no])
+                        misty.speak(quiz_data["transitions"][question_no])
 
 
 if __name__ == "__main__":
         ip_address = "192.168.0.41"
         # Create an instance of a robot
         misty = Robot(ip_address)
-        mt5_question1()
+        mt5_quiz_session()
+
+
 
         # current_response = misty.move_arms(30, 20)
         # current_response = misty.move_head(-40, 0, 0, 80)
